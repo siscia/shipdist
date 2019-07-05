2e4250a1bada3215cceb1099e7babfd3c25cf720
Simone Mosciatti <simone.mosciatti@cern.ch>
2019-07-05 11:43:56 +0200

    Work on python3 compatibility

--- fairship.sh
+++ fairship.sh
@@ -105,7 +105,7 @@ cmake $SOURCEDIR                                                 \
       -DEVTGEN_INCLUDE_DIR=$EVTGEN_ROOT/include                  \
       -DEVTGEN_LIBRARY_DIR=$EVTGEN_ROOT/lib                      \
       -DPYTHON_LIBRARY=$PYTHON_ROOT/lib                          \
-      -DPYTHON_INCLUDE_DIR=$PYTHON_ROOT/include/python2.7        \
+      -DPYTHON_INCLUDE_DIR=$PYTHON_ROOT/include/python3.6m/      \
       -DPythia6_LIBRARY_DIR=$PYTHIA6_ROOT/lib                    \
       -DPYTHIA8_DIR=$PYTHIA_ROOT                                 \
       -DPYTHIA8_INCLUDE_DIR=$PYTHIA_ROOT/include                 \
